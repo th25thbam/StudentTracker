62d1a256918d1e81de21a923421d15f8dd398f09
Atharv <97350325+th25thbam@users.noreply.github.com>
2026-09-18 20:42:31 +0530

    Add files via upload

---
+++ model/Person.java
@@ -0,0 +1,14 @@
+package model;
+
+public abstract class Person {
+    private String id;
+    private String name;
+
+    public Person(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+}
